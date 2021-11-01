1814a5246c69630d8a50259440189d57cefd7c4a
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-11-01 00:39:00 -0400

    Remove ObjectSet

--- foundry/game/ObjectSet.py
+++ foundry/game/ObjectSet.py
@@ -1,18 +1,16 @@
 from foundry.game.ObjectDefinitions import ObjectDefinition, load_object_definitions
+from foundry.smb3parse.constants import TILESET_ENDINGS, TILESET_NAMES
 from foundry.smb3parse.objects.object_set import ENEMY_ITEM_OBJECT_SET
-from foundry.smb3parse.objects.object_set import ObjectSet as _ObjectSet
 
 
 class ObjectSet:
     def __init__(self, object_set_number: int):
-        self._internal_object_set = _ObjectSet(object_set_number)
-
-        self.number = self._internal_object_set.number
+        self.number = object_set_number
 
         if self.number == ENEMY_ITEM_OBJECT_SET:
             self.name = "Enemy Object Set"
         else:
-            self.name = self._internal_object_set.name
+            self.name = TILESET_NAMES[self.number]
 
         self.definitions = load_object_definitions(self.number)
 
@@ -32,7 +30,7 @@ class ObjectSet:
         if self.number == ENEMY_ITEM_OBJECT_SET:
             raise ValueError(f"This method shouldn't be called for the {self.name}")
 
-        return self._internal_object_set.ending_graphic_offset
+        return TILESET_ENDINGS[self.number]
 
     def get_object_byte_length(self, domain: int, object_id: int) -> int:
         if self.number == ENEMY_ITEM_OBJECT_SET:
