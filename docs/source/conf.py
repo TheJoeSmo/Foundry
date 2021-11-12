3e36e458b8675e452d442db021bbf11cd91b9639
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-11-12 18:10:52 -0500

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = "2021, TheJoeSmo"
 author = "TheJoeSmo"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.4"
+release = "0.2.8"
 
 
 # -- General configuration ---------------------------------------------------
