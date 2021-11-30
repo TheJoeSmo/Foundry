114663d90e3fbe68703d7150ed30ba7f5517f3b5
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-11-30 02:53:51 -0500

    Fix warning test to use pydantic model

--- tests/gui/test_warning_list.py
+++ tests/gui/test_warning_list.py
@@ -7,7 +7,7 @@ def test_enemy_names(main_window):
 
     enemy_object_set = ObjectSet(ENEMY_ITEM_OBJECT_SET)
 
-    enemy_names = [obj_def.description for obj_def in enemy_object_set.definitions]
+    enemy_names = [obj_def.description for obj_def in enemy_object_set.definitions.__root__]
 
     for enemy_name in warning_list._enemy_dict.keys():
         assert enemy_name in enemy_names
