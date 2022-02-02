95cb6acb3c98373b2eb10f980852dc6718c4e799
Smith14942 <joesmo.joesmo12@gmail.com>
2022-02-02 18:43:26 -0500

    Refactor Palette to mutable and immutable variances

--- foundry/core/palette/Palette.py
+++ foundry/core/palette/Palette.py
@@ -1,4 +1,5 @@
-from typing import Protocol
+from abc import ABC, abstractmethod
+from typing import Protocol, Sequence, Type, TypeVar
 
 from attr import attrs
 from PySide6.QtGui import QColor
@@ -7,8 +8,8 @@ from foundry.core.palette import COLORS_PER_PALETTE, NESPalette
 from foundry.game.File import ROM
 
 
-class MutablePaletteProtocol(Protocol):
-    color_indexes: list[int]
+class PaletteProtocol(Protocol):
+    color_indexes: Sequence[int]
 
     def __bytes__(self) -> bytes:
         ...
@@ -16,17 +17,30 @@ class MutablePaletteProtocol(Protocol):
     def __getitem__(self, item: int) -> int:
         ...
 
+    @property
+    def colors(self) -> Sequence[QColor]:
+        ...
+
+
+class MutablePaletteProtocol(PaletteProtocol, Protocol):
     def __setitem__(self, key: int, value: int):
         ...
 
-    @property
-    def colors(self) -> list[QColor]:
+
+class HashablePaletteProtocol(PaletteProtocol, Protocol):
+    def __hash__(self) -> int:
         ...
 
 
-@attrs(slots=True, auto_attribs=True)
-class MutablePalette:
-    color_indexes: list[int]
+_T = TypeVar("_T", bound="AbstractPalette")
+
+
+class AbstractPalette(ABC):
+    """
+    An abstract implementation of the primary methods of a Palette, regardless of color_indexes.
+    """
+
+    color_indexes: Sequence[int]
 
     def __bytes__(self) -> bytes:
         return bytes([i & 0xFF for i in self.color_indexes])
@@ -34,23 +48,54 @@ class MutablePalette:
     def __getitem__(self, item: int) -> int:
         return self.color_indexes[item]
 
-    def __setitem__(self, key: int, value: int):
-        self.color_indexes[key] = value
+    @property
+    def colors(self) -> Sequence[QColor]:
+        return [NESPalette[c & 0x3F] for c in self.color_indexes]
 
     @classmethod
-    def as_empty(cls):
+    @abstractmethod
+    def from_values(cls: Type[_T], *values: int) -> _T:
         """
-        Makes an empty palette of default values
+        A generalized way to create itself from a series of values.
 
         Returns
         -------
-        MutablePalette
-            A palette filled with default values.
+        AbstractPalette
+            The created palette from the series.
         """
-        return cls([0, 0, 0, 0])
+        ...
 
     @classmethod
-    def from_rom(cls, address: int):
+    def from_palette(cls: Type[_T], palette: PaletteProtocol) -> _T:
+        """
+        Generates a AbstractPalette of this type from another PaletteProtocol.
+
+        Parameters
+        ----------
+        palette : PaletteProtocol
+            The palette to be converted to this type.
+
+        Returns
+        -------
+        AbstractPalette
+            A palette that is equal to the original palette.
+        """
+        return cls.from_values(*palette.color_indexes)
+
+    @classmethod
+    def as_empty(cls: Type[_T]) -> _T:
+        """
+        Makes an empty palette of default values.
+
+        Returns
+        -------
+        AbstractPalette
+            A palette filled with default values.
+        """
+        return cls.from_values(0, 0, 0, 0)
+
+    @classmethod
+    def from_rom(cls: Type[_T], address: int) -> _T:
         """
         Creates a palette from an absolute address in ROM.
 
@@ -61,11 +106,28 @@ class MutablePalette:
 
         Returns
         -------
-        MutablePalette
+        AbstractPalette
             The palette that represents the absolute address in ROM.
         """
-        return cls([int(i) for i in ROM().read(address, COLORS_PER_PALETTE)])
+        return cls.from_values(*tuple(int(i) for i in ROM().read(address, COLORS_PER_PALETTE)))
 
-    @property
-    def colors(self) -> list[QColor]:
-        return [NESPalette[c & 0x3F] for c in self.color_indexes]
+
+@attrs(slots=True, auto_attribs=True)
+class MutablePalette(AbstractPalette):
+    color_indexes: list[int]
+
+    def __setitem__(self, key: int, value: int):
+        self.color_indexes[key] = value
+
+    @classmethod
+    def from_values(cls: Type[_T], *values: int) -> _T:
+        return cls(list(values))  # type: ignore
+
+
+@attrs(slots=True, auto_attribs=True, frozen=True, hash=True)
+class Palette(AbstractPalette):
+    color_indexes: tuple[int, ...]
+
+    @classmethod
+    def from_values(cls: Type[_T], *values: int) -> _T:
+        return cls(values)  # type: ignore
