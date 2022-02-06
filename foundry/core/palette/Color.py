c72cf4ad6fb2508efa26104edb04057754a9f710
Smith14942 <joesmo.joesmo12@gmail.com>
2022-02-05 20:49:30 -0500

    Add ColorPalette

--- foundry/core/palette/Color.py
+++ foundry/core/palette/Color.py
@@ -1,7 +1,7 @@
 from functools import cached_property
 from typing import Protocol
 
-from attr import attrs, field
+from attr import attrs, field, validators
 from pydantic import BaseModel, validator
 from PySide6.QtGui import QColor
 
@@ -13,16 +13,18 @@ class ColorProtocol(Protocol):
     alpha: int
 
 
+def _check_in_color_range(inst, attr, value):
+    if not 0 <= value <= 0xFF:
+        raise ValueError(f"{value} is not inside the range 0-255")
+    return value
+
+
 @attrs(slots=True, frozen=True, eq=True, hash=True)
 class Color:
-    red: int = field(validator=[attrs.validators.instance_of(int), attrs.validators.le(0x100), attrs.validators.ge(0)])
-    blue: int = field(validator=[attrs.validators.instance_of(int), attrs.validators.le(0x100), attrs.validators.ge(0)])
-    green: int = field(
-        validator=[attrs.validators.instance_of(int), attrs.validators.le(0x100), attrs.validators.ge(0)]
-    )
-    alpha: int = field(
-        default=255, validator=[attrs.validators.instance_of(int), attrs.validators.le(0x100), attrs.validators.ge(0)]
-    )
+    red: int = field(validator=[validators.instance_of(int), _check_in_color_range])
+    blue: int = field(validator=[validators.instance_of(int), _check_in_color_range])
+    green: int = field(validator=[validators.instance_of(int), _check_in_color_range])
+    alpha: int = field(default=255, validator=[validators.instance_of(int), _check_in_color_range])
 
     @cached_property
     def qcolor(self) -> QColor:
@@ -31,12 +33,21 @@ class Color:
 
 class PydanticColor(BaseModel):
     red: int
-    blue: int
     green: int
-    alpha: int
+    blue: int
+    alpha: int = 255
 
-    @validator("red", "blue", "green", "alpha")
+    # Enable cached property to be ignored by Pydantic
+    class Config:
+        arbitrary_types_allowed = True
+        keep_untouched = (cached_property,)
+
+    @validator("red", "blue", "green", "alpha", allow_reuse=True)
     def check_color_range(cls, v):
         if not 0 <= v <= 0xFF:
             raise ValueError(f"{v} is not inside the range 0-255")
         return v
+
+    @cached_property
+    def color(self) -> ColorProtocol:
+        return Color(self.red, self.green, self.blue, self.alpha)
