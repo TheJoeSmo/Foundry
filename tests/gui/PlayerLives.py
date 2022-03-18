6644e58cc0f078bfffd9f916cb739d5ccbe97007
wilsonpenn <72536367+wilsonpenn@users.noreply.github.com>
2022-03-18 00:08:32 -0400

    add more tests for validity of state changes.  Fix object reference vs copy issue.

--- tests/gui/PlayerLives.py
+++ tests/gui/PlayerLives.py
@@ -20,22 +20,66 @@ def initialState(store : Store):
     assert default_starting_lives == store.getState().starting_lives
     assert default_continue_lives == store.getState().continue_lives
 
-def test_subscribe():
+def test_subscribeOnInvalidAction():
     callback_called = False
-    store = Store(Rom(bytearray([0] * 0x50000)))
+    store = defaultStore()
     callback = CallbackTester()
     store.subscribe(callback.function)
     assert 0 == callback.called
 
     store.dispatch(Action("", None))
+    assert 0 == callback.called
+
+def test_subscribeOnValidActionStateChange():
+    callback_called = False
+    store = defaultStore()
+    callback = CallbackTester()
+    store.subscribe(callback.function)
+    assert 0 == callback.called
+
+    store.dispatch(Action(ActionNames.starting_lives, 1))
     assert 1 == callback.called
+
+def test_subscribeOnValidActionNoStateChange():
+    callback_called = False
+    store = defaultStore()
+    callback = CallbackTester()
+    store.subscribe(callback.function)
+    assert 0 == callback.called
+
+    store.dispatch(Action(ActionNames.starting_lives, default_starting_lives))
+    assert 0 == callback.called
 
 def test_startingLivesAction():
     startingLivesAction(defaultStore(), 1)
 
-def startingLivesAction(store: Store, updated_starting_lives: int):
+def startingLivesAction(store: Store, updated_starting_lives):
     store.dispatch(Action(ActionNames.starting_lives, updated_starting_lives))
     assert updated_starting_lives == store.getState().starting_lives
+
+def test_startingLivesActionInvalidType():
+    store = defaultStore()
+    store.dispatch(Action(ActionNames.starting_lives, "P"))
+    assert default_starting_lives == store.getState().starting_lives
+
+def test_startingLivesActionInvalidValues():
+    store = defaultStore()
+    store.dispatch(Action(ActionNames.starting_lives, -1))
+    assert default_starting_lives == store.getState().starting_lives
+    store.dispatch(Action(ActionNames.starting_lives, 100))
+    assert default_starting_lives == store.getState().starting_lives
+
+def test_continueLivesActionInvalidType():
+    store = defaultStore()
+    store.dispatch(Action(ActionNames.continue_lives, "P"))
+    assert default_continue_lives == store.getState().continue_lives
+
+def test_continueLivesActionInvalidValues():
+    store = defaultStore()
+    store.dispatch(Action(ActionNames.continue_lives, -1))
+    assert default_continue_lives == store.getState().continue_lives
+    store.dispatch(Action(ActionNames.continue_lives, 100))
+    assert default_continue_lives == store.getState().continue_lives
 
 def test_continueLivesAction():
     updated_continue_lives = 2
