c841737c96aa71e071d6b585b8c6b9dc828def55
wilsonpenn <72536367+wilsonpenn@users.noreply.github.com>
2022-03-19 22:39:53 -0400

    add test for preamble

--- foundry/smb3parse/util/code_edit_area.py
+++ foundry/smb3parse/util/code_edit_area.py
@@ -5,9 +5,14 @@ from foundry.smb3parse.util.rom import Rom
 class CodeEditArea:
     start_address: int
     length: int
-    pre_guard: bytearray
-    post_guard: bytearray
+    preamble: bytearray
+    postamble: bytearray
+
+    def __validate(rom: Rom, address: int, data: bytearray):
+        if len(data) == 0: return True
+        return data == rom.read(address, len(data))
 
     def isValid(self, rom: Rom):
+        if CodeEditArea.__validate(rom, self.start_address - len(self.preamble), self.preamble) == False: return False
         return True
     
