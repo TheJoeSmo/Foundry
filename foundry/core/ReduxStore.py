1a47dedd0358feb617192d2bc8cb30414643f5e3
wilsonpenn <72536367+wilsonpenn@users.noreply.github.com>
2022-03-21 23:36:32 -0400

    abstract out the ReduxStore

---
+++ foundry/core/ReduxStore.py
@@ -0,0 +1,39 @@
+from typing import TypeVar, Generic
+import copy
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+@dataclass
+class Action:
+    type: str
+    payload: any
+
+S = TypeVar('S')
+
+class ReduxStore(ABC, Generic[S]):
+    state : S = None
+    subscribers = []
+
+    def __init__(self, state: S):
+        self.state = state
+
+    def getState(self) -> S:
+        return self.state
+
+    def dispatch(self, action: Action):
+        oldState = copy.deepcopy(self.state)
+        self.state = self.reduce(copy.deepcopy(self.state), action)
+
+        if self.state != oldState:
+            self.__notifySubscribers()
+
+    def __notifySubscribers(self):
+        for subscriber in self.subscribers:
+            subscriber()
+
+    @abstractmethod
+    def reduce(self, state:S, action: Action) -> S:
+        pass
+
+    def subscribe(self, subscriber):
+        self.subscribers.append(subscriber)
