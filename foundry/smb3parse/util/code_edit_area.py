14d3b18689b3438101e829ee23b545c4a6e8cf8a
wilsonpenn <72536367+wilsonpenn@users.noreply.github.com>
2022-03-25 22:08:22 -0400

    refactor

--- foundry/smb3parse/util/code_edit_area.py
+++ foundry/smb3parse/util/code_edit_area.py
@@ -3,16 +3,17 @@ from foundry.smb3parse.util.rom import Rom
 
 @dataclass
 class CodeEditArea:
+    rom: Rom
     address: int
     length: int
     prefix: bytearray
     postfix: bytearray
 
-    def __validateAffix(rom: Rom, testAddress: int, data: bytearray):
+    def __validateAffix(self, testAddress: int, data: bytearray):
         if len(data) == 0: return True
-        return data == rom.read(testAddress, len(data))
+        return data == self.rom.read(testAddress, len(data))
 
-    def isValid(self, rom: Rom):
-        if not CodeEditArea.__validateAffix(rom, self.address - len(self.prefix), self.prefix): return False
-        return CodeEditArea.__validateAffix(rom, self.address + self.length, self.postfix)
+    def isValid(self):
+        if not self.__validateAffix(self.address - len(self.prefix), self.prefix): return False
+        return self.__validateAffix(self.address + self.length, self.postfix)
     
