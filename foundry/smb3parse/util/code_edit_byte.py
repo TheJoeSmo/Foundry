f1877f216a4b6a71ecbf2dbdfa09c03377a602ae
wilsonpenn <72536367+wilsonpenn@users.noreply.github.com>
2022-03-27 17:43:06 -0400

    remove unused imports. fix issues in documentation of redux_store.py add documentation to the code_edit*.py modules

--- foundry/smb3parse/util/code_edit_byte.py
+++ foundry/smb3parse/util/code_edit_byte.py
@@ -1,14 +1,24 @@
 from foundry.smb3parse.util.code_edit import CodeEdit
 from foundry.smb3parse.util.rom import Rom
 
-class CodeEditByte(CodeEdit):
+class CodeEditByte(CodeEdit[int]):
+    """ Edit a single byte of memory in the ROM. """
     def __init__(self, rom: Rom, start_address: int, prefix: bytearray, postfix: bytearray):
         super().__init__(rom, start_address, 1, prefix, postfix)
 
     def read(self) -> int:
-        if not self.is_valid(): return
+        """ Reads the target byte out of the ROM.
+        
+        This reads the byte at the target code address if both the prefix and
+        postfix are valid.  If they are not valid, returns None.
+        """
+        if not self.is_valid(): return None
         return self.rom.read(self.address, self.length)[0]
 
     def write(self, value: int):
+        """ Writes the specified byte to the target address if valid. 
+        
+        If the prefix or postfix or not valid, the write is not allowed.
+        """
         if not self.is_valid(): return
         self.rom.write(self.address, [value])
--- tests/smb3parse/util/test_code_edit.py
+++ tests/smb3parse/util/test_code_edit.py
@@ -5,6 +5,13 @@ long_prefix = bytearray([0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF])
 long_postfix = bytearray([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08])
 empty_affix = bytearray([])
 
+class CodeEditTest(CodeEdit[bool]):
+    def read(self) -> bool:
+        return True
+
+    def write(self, value: bool):
+        return
+
 def get_test_rom() -> Rom:
     rom = Rom(bytearray([0] * 0x50000))
     rom.write(0xF8, long_prefix)
@@ -12,26 +19,26 @@ def get_test_rom() -> Rom:
     return rom
 
 def test_is_valid_no_guards():
-    test_area = CodeEdit(get_test_rom(), 0x100, 1, empty_affix, empty_affix)
+    test_area = CodeEditTest(get_test_rom(), 0x100, 1, empty_affix, empty_affix)
     assert True == test_area.is_valid()
 
 def test_is_valid_1_byte_prefix_invalid():
-    test_area = CodeEdit(get_test_rom(), 0x100, 1, bytearray([0x11]), empty_affix)
+    test_area = CodeEditTest(get_test_rom(), 0x100, 1, bytearray([0x11]), empty_affix)
     assert False == test_area.is_valid()
 
 def test_is_valid_1_byte_postfix_invalid():
-    test_area = CodeEdit(get_test_rom(), 0x100, 1, empty_affix, bytearray([0x11]))
+    test_area = CodeEditTest(get_test_rom(), 0x100, 1, empty_affix, bytearray([0x11]))
     assert False == test_area.is_valid()
 
 def test_long_prefix():
-    test_area = CodeEdit(get_test_rom(), 0x100, 1, long_prefix, empty_affix)
+    test_area = CodeEditTest(get_test_rom(), 0x100, 1, long_prefix, empty_affix)
     assert True == test_area.is_valid()
 
 def test_long_postfix():
-    test_area = CodeEdit(get_test_rom(), 0x100, 1, empty_affix, long_postfix)
+    test_area = CodeEditTest(get_test_rom(), 0x100, 1, empty_affix, long_postfix)
     assert True == test_area.is_valid()
 
 def test_full_affix():
-    test_area = CodeEdit(get_test_rom(), 0x100, 1, long_prefix, long_postfix)
+    test_area = CodeEditTest(get_test_rom(), 0x100, 1, long_prefix, long_postfix)
     assert True == test_area.is_valid()
     
