eae91010ba3f2300c0360e764533ab23fcc95658
TheJoeSmo <joesmo.joesmo12@gmail.com>
2022-08-06 00:15:28 -0400

    Remove comparision tests
    The comparison tests have not been part of the test suit in months due
    to them being machine dependant.  It also required a long and tedious
    interaction between the developer and the outputs, taking upwards
    of an hour.  It was also extremely slow, rendering thousands of images.
    Nor did it actually reflect a benefit to the game, as these did not
    ensure that the editor reflected the game.  For this reason, the
    tests have been removed.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,8 @@
 import os
-import subprocess
-from pathlib import Path
 
 import pytest
-from git.repo.base import Repo
 from PySide6.QtGui import QPixmap
 
-from foundry.game.File import ROM
-from foundry.game.level.Level import Level
-from foundry.smb3parse.objects.object_set import PLAINS_OBJECT_SET
 from tests.gui import ApprovalDialog
 
 level_1_1_object_address = 0x1FB92
@@ -16,32 +10,6 @@ level_1_1_enemy_address = 0xC537 + 1
 
 level_1_2_object_address = 0x20F3A
 level_1_2_enemy_address = 0xC6BA + 1
-
-
-@pytest.fixture
-def level(rom_singleton, qtbot):
-    return Level("PydanticLevel 1-1", level_1_1_object_address, level_1_1_enemy_address, PLAINS_OBJECT_SET)
-
-
-@pytest.fixture(scope="module", autouse=True)
-def rom_singleton():
-    rom_path = Path(__file__).parent.resolve() / "artifacts"
-
-    if not rom_path.exists() and not rom_path.is_dir():
-        Repo.clone_from("https://github.com/Drakim/smb3", rom_path)
-
-    try:
-        subprocess.run([rom_path / "asm6.exe", "smb3.asm"], cwd=rom_path)
-    except PermissionError:
-        # Try Linux alternative
-        subprocess.run(["chmod", "+x", rom_path / "asm6.exe"])  # Give file execution perms
-        subprocess.run(["wine", rom_path / "asm6.exe", "smb3.asm"], cwd=rom_path)
-
-    test_rom_path = rom_path / "smb3.bin"
-
-    rom = ROM(str(test_rom_path))
-
-    yield rom
 
 
 def compare_images(image_name: str, ref_image_path: str, gen_image: QPixmap):
--- tests/game/conftest.py
+++ tests/game/conftest.py
@@ -1,15 +1,14 @@
 import subprocess
 from pathlib import Path
 
-import pytest
 from git.repo.base import Repo
+from pytest import fixture
 
-from foundry.smb3parse.levels.world_map import WorldMap
-from foundry.smb3parse.util.rom import Rom
+from foundry.game.File import ROM
 
 
-@pytest.fixture(scope="session")
-def rom():
+@fixture(scope="session", autouse=True)
+def rom_singleton():
     rom_path = Path(__file__).parent.resolve() / "artifacts"
 
     if not rom_path.exists() and not rom_path.is_dir():
@@ -20,17 +19,9 @@ def rom():
     except PermissionError:
         # Try Linux alternative
         subprocess.run(["chmod", "+x", rom_path / "asm6.exe"])  # Give file execution perms
+
     test_rom_path = rom_path / "smb3.bin"
 
-    with open(test_rom_path, "rb") as rom_file:
-        yield Rom(bytearray(rom_file.read()))
+    rom = ROM(str(test_rom_path))
 
-
-@pytest.fixture
-def world_1(rom):
-    return WorldMap.from_world_number(rom, 1)
-
-
-@pytest.fixture
-def world_8(rom):
-    return WorldMap.from_world_number(rom, 8)
+    yield rom
