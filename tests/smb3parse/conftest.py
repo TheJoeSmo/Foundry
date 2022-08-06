e0aa9bad703a1def4d087ab27de125286642815d
TheJoeSmo <joesmo.joesmo12@gmail.com>
2022-08-06 00:14:44 -0400

    Fix test permission error on Linux

--- tests/smb3parse/conftest.py
+++ tests/smb3parse/conftest.py
@@ -8,14 +8,19 @@ from foundry.smb3parse.levels.world_map import WorldMap
 from foundry.smb3parse.util.rom import Rom
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def rom():
     rom_path = Path(__file__).parent.resolve() / "artifacts"
 
     if not rom_path.exists() and not rom_path.is_dir():
         Repo.clone_from("https://github.com/Drakim/smb3", rom_path)
 
-    subprocess.run([rom_path / "asm6.exe", "smb3.asm"], cwd=rom_path)
+    try:
+        subprocess.run([rom_path / "asm6.exe", "smb3.asm"], cwd=rom_path)
+    except PermissionError:
+        # Try Linux alternative
+        subprocess.run(["chmod", "+x", rom_path / "asm6.exe"])  # Give file execution perms
+        subprocess.run(["wine", rom_path / "asm6.exe", "smb3.asm"], cwd=rom_path)
     test_rom_path = rom_path / "smb3.bin"
 
     with open(test_rom_path, "rb") as rom_file:
