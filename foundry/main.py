86d35dd63a9429012a49a10f9cd9cde763b2a253
TheJoeSmo <joesmo.joesmo12@gmail.com>
2022-09-01 01:08:12 -0400

    Remove pretty errors
    Pretty errors is an extra dependency which we did not need.  It also made searching errors harder due to its 'pretty format'.

--- foundry/main.py
+++ foundry/main.py
@@ -5,7 +5,6 @@ import sys
 import traceback
 from argparse import ArgumentParser, BooleanOptionalAction
 
-import pretty_errors  # noqa: F401 is used to provide nicer messages throughout the project.
 from PySide6.QtWidgets import QApplication, QMessageBox
 
 from foundry import auto_save_rom_path, github_issue_link
