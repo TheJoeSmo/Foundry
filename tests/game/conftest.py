acd81cdec30603ab0500b2e3c5c1c796d9eabe10
TheJoeSmo <joesmo.joesmo12@gmail.com>
2022-11-02 14:52:40 -0400

    Refactor ROM required tests to optimize localized testing

--- tests/game/conftest.py
+++ tests/game/conftest.py
@@ -1,5 +1,6 @@
 import subprocess
 from pathlib import Path
+from shutil import copyfile
 
 from git.repo.base import Repo
 from pytest import fixture
@@ -9,19 +10,21 @@ from foundry.game.File import ROM
 
 @fixture(scope="session", autouse=True)
 def rom_singleton():
-    rom_path = Path(__file__).parent.resolve() / "artifacts"
+    rom_directory: Path = Path(__file__).parent.resolve() / "artifacts"
+    rom_path: Path = rom_directory / "smb3.bin"
+    test_rom_path: Path = rom_directory / "smb3_test.nes"
 
-    if not rom_path.exists() and not rom_path.is_dir():
-        Repo.clone_from("https://github.com/Drakim/smb3", rom_path)
+    if not rom_directory.exists() and not rom_directory.is_dir():
+        Repo.clone_from("https://github.com/Drakim/smb3", rom_directory)
 
-    try:
-        subprocess.run([rom_path / "asm6.exe", "smb3.asm"], cwd=rom_path)
-    except PermissionError:
-        # Try Linux alternative
-        subprocess.run(["chmod", "+x", rom_path / "asm6.exe"])  # Give file execution perms
+    if not rom_path.exists():
+        try:
+            subprocess.run([rom_directory / "asm6.exe", "smb3.asm"], cwd=rom_directory)
+        except PermissionError:
+            # Try Linux alternative
+            subprocess.run(["chmod", "+x", rom_directory / "asm6.exe"])  # Give file execution perms
 
-    test_rom_path = rom_path / "smb3.bin"
-
+    copyfile(rom_path, test_rom_path)
     rom = ROM(str(test_rom_path))
 
     yield rom
